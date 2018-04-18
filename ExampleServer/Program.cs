c5e75b9737362baebd47cb1cf5b84328619e69d4
Martin Wangen <wangen93@hotmail.com>
2018-04-18 11:38:40 +0200

    Implemented Gzip compression and decompression

--- ExampleServer/Program.cs
+++ ExampleServer/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.IO;
 using HTTP2Server;
+using lib;
 
 namespace ExampleServer
 {
@@ -9,6 +11,15 @@ namespace ExampleServer
         {
             Console.WriteLine("Hello World!");
             HTTP2Server.Server.test();
+
+            //Zip test
+            DirectoryInfo di = new DirectoryInfo(@"C:\Users\Martin Wangen\source\repos\HTTP-2-server\ExampleServer\WebApp\");
+            FileInfo fi = new FileInfo(@"C:\Users\Martin Wangen\source\repos\HTTP-2-server\ExampleServer\WebApp\temp\index.html.gz");
+            ZipStream.Compress(di);
+            ZipStream.Decompress(fi);
+
+
+            Console.ReadLine();
         }
     }
 }
