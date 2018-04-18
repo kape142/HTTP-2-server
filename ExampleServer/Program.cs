eeeb81fa9491bd04a094c030ca957025be0ea5da
Karl Peter <[kape142@hotmail.com]>
2018-04-18 09:49:14 +0200

    init

---
+++ ExampleServer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using HTTP2Server;
+
+namespace ExampleServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            HTTP2Server.Server.test();
+        }
+    }
+}
---
+++ lib/Server.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HTTP2Server
+{
+    public class Server
+    {
+        public static void test()
+        {
+            Console.WriteLine("test");
+        }
+    }
+}
