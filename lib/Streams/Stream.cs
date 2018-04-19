624dfdd4d7ce8e2cbb35be4c902f53287127c3e0
Martin Wangen <wangen93@hotmail.com>
2018-04-19 13:16:27 +0200

    Steamclass created

---
+++ lib/Streams/Stream.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lib
+{
+    public enum StreamState
+    {
+        Idle,
+        ReservedLocal,
+        ReservedRemote,
+        Open,
+        HalfClosedLocal,
+        HalfClosedRemote,
+        Closed
+
+    }
+    class Stream
+    {
+        public uint Id
+        {
+            get;
+        }
+        public uint Priority
+        {
+            get; set;
+        }
+        public StreamState State
+        {
+            get;set;
+        }
+
+        //TODO
+        void close()
+        {
+            State = StreamState.Closed;
+        }    
+
+
+    }
+}
